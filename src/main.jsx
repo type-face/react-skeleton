d3def2fdf14e290207806ee3038091eedb034e51
Rob Ring <rjm.ring@gmail.com>
2016-03-27 19:59:00 -0500

    removed more test code

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,2 @@
 var React = require('react');
 var ReactDom = require('react-dom');
-var List = require('./components/List.jsx');
-
-ReactDom.render(<List />, document.getElementById('ingredients'));
